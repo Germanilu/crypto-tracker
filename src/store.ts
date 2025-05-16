75dfe3ebf7bf37eb374950816c5fc675348d8b0b
Luciano Germani <lucianogermani93@gmail.com>
2025-05-16 16:12:10 +0200

    Feat: Tipado de respuesta api

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,33 @@
 import { create } from 'zustand'
+import axios from 'axios';
+import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema';
+import type { CryptoCurrency } from './types';
 
-export const useCryptoStore = create(() => ({
-    fetchCryptos : () => {
-        console.log('desde fetchcryptos')
+
+type CryptoStore = {
+    cryptocurrencies: CryptoCurrency[]
+    fetchCryptos: () => Promise<void>
+}
+
+async function getCrypto(){
+    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
+    const {data:{Data}} = await axios(url)
+    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
+    if(result.success){
+        return result.data
+    }
+}
+
+//Creacion del store con sus acciones
+export const useCryptoStore = create<CryptoStore>((set) => ({
+
+    cryptocurrencies: [],
+
+    //Acciones
+    fetchCryptos : async () => {
+        const cryptocurrencies = await getCrypto()
+        set(() => ({
+            cryptocurrencies
+        }))
     }
 }));
