4afd9ec92453f6e6f35652a0e0523987b363c50f
Luciano Germani <lucianogermani93@gmail.com>
2025-05-16 15:26:14 +0200

    Feat: Crypto Tracker project Ts+Zod+Zustand

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import CryptoSearchForm from "./components/CryptoSearchForm"
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="app-title">
+          Cotizador de <span>Cryptomonedas</span>
+        </h1>
+
+        <div className="content">
+          <CryptoSearchForm/>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CryptoSearchForm.tsx
@@ -0,0 +1,37 @@
+import { currencies } from "../data";
+
+export default function CryptoSearchForm() {
+  return (
+    <form className="form">
+      <div className="field">
+        <label htmlFor="currency">Moneda:</label>
+        <select
+          name="currency"
+          id="currency"
+        >
+          <option value="">-- Seleccione --</option>
+        </select>
+      </div>
+
+      <div className="field">
+        <label htmlFor="cryptocurrency">Cryptomoneda:</label>
+        <select
+          name="cryptocurrency"
+          id="cryptocurrency"
+        >
+          <option value="">-- Seleccione --</option>
+          {currencies.map(currency => (
+            <option
+              value={currency.code}
+              key={currency.code}
+            >
+              {currency.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input type="submit" value='Cotizar' />
+    </form>
+  )
+}
